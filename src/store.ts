a651a26dd7cc14c61e51e22b345430388a6ba4fd
Segundavid-Dev <segdavid03@gmail.com>
2025-05-10 15:41:03 +0100

    redux-typescript

---
+++ src/store.ts
@@ -0,0 +1,47 @@
+type initialStateTypes = {
+  loan: number;
+  balance: number;
+  loanPurpose: string;
+};
+
+type Action =
+  | { type: "account/deposit"; payload: number }
+  | { type: "account/withdraw"; payload: number }
+  | { type: "account/requestLoan"; payload: number }
+  | { type: "account/payLoan" };
+
+const initialState: initialStateTypes = {
+  loan: 0,
+  balance: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
