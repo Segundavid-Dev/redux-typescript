ed7169a8df417df4cdfa3ef3a8a279c63258bd05
Segundavid-Dev <segdavid03@gmail.com>
2025-05-12 17:30:37 +0100

    finally gotten how to type reducers, dispatch function, types and payload in typescript

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 type initialStateTypes = {
   loan: number;
   balance: number;
@@ -45,3 +47,64 @@ function reducer(state = initialState, action: Action) {
       return state;
   }
 }
+
+// create our store in react
+const store = createStore(reducer);
+// from this store, we can dispatch actions
+store.dispatch(deposit(500));
+
+// Action creators
+function deposit(amount: number): Action {
+  return { type: "account/deposit", payload: amount };
+}
+
+// specifying the return type of this function in typescript
+
+type initialStateCustomersTypes = {
+  fullName: string;
+  nationalID: string;
+  createdAt: string;
+};
+
+// Creating more customers, created types for this
+const initialStateCustomers: initialStateCustomersTypes = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+type customerActionTypes = {
+  type: "customer/createCustomer";
+  payload: { fullName: string; nationalID: string; createdAt: string };
+};
+
+function customerReducer(
+  state = initialStateCustomers,
+  action: customerActionTypes
+) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload?.fullName,
+        nationalID: action.payload?.nationalID,
+        createdAt: action.payload?.createdAt,
+      };
+
+    default:
+      return state;
+  }
+}
+
+function createCustomer(
+  fullName: string,
+  nationalID: string
+): customerActionTypes {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+const customerStore = createStore(customerReducer);
+customerStore.dispatch(createCustomer("Segun David", "20223697"));
